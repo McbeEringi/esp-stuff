4681f2def891cf620bfdd12bd405effa5f79b2c6
McbeEringi <git@mcbeeringi.dev>
2025-12-16 12:27:50 +0900

    -

--- escpos/src/main.cpp
+++ escpos/src/main.cpp
@@ -1,28 +1,107 @@
 #include <Arduino.h>
+#include <LittleFS.h>
+#include "wlan.h"
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoOTA.h>
+
+#define FSYS LittleFS
+#define NAME "esp-tp"
+#define PASS "mcbeeringi"
+
+#define PUB_DIR "/public"
+#define PVT_DIR "/private"
+#define WIFI_CFG "/private/wifi.tsv"
+
+#define NPDI 1
+#define BTNA 2
+#define BTNB 3
+
+#define P_INIT "\x1b\x40"
+#define P_SELF "\x1b##SELF"
+
 HardwareSerial escpos(0);
-uint8_t btn=0;
+
+static AsyncWebServer svr(80);
+static AsyncWebSocket ws("/ws");
+
+bool isAPmode=false;
+
+struct Btn{bool a;bool b;};
+Btn btn={false,false};
+void ARDUINO_ISR_ATTR isr_btn_a(void *arg){(static_cast<Btn *>(arg))->a=true;}
+void ARDUINO_ISR_ATTR isr_btn_b(void *arg){(static_cast<Btn *>(arg))->b=true;}
+
+
 void setup(){
+	neopixelWrite(NPDI,16,0,0);
+	FSYS.begin();
 	Serial.begin(115200);
 	escpos.begin(115200);
-	pinMode(2,INPUT_PULLUP);
-	pinMode(3,INPUT_PULLUP);
+	escpos.write(P_INIT);
+	pinMode(BTNA,INPUT_PULLUP);
+	attachInterruptArg(BTNA,isr_btn_a,&btn,FALLING);
+	pinMode(BTNB,INPUT_PULLUP);
+	attachInterruptArg(BTNB,isr_btn_b,&btn,FALLING);
 	delay(1000);
-	escpos.write("\x1b\x40");
-	// escpos.write("\x1b##SELF");
+
+	isAPmode=wlan(
+		NPDI,
+		FSYS,WIFI_CFG,
+		NAME,PASS
+	);
+
+	for(uint8_t i=8;i;--i){
+		if(isAPmode)neopixelWrite(NPDI,0,16,16);
+		else neopixelWrite(NPDI,0,16,0);
+		delay(62);
+		neopixelWrite(NPDI,0,0,0);
+		delay(62);
+	}
+
+	ArduinoOTA
+		.setHostname(NAME).setPassword(PASS)
+		.onStart([](){ws.enable(false);ws.textAll("OTA update started.");ws.closeAll();FSYS.end();})
+		.onProgress([](unsigned int x,unsigned int a){neopixelWrite(NPDI,(a-x)*64/a,x*64/a,0);})
+		.onError([](ota_error_t e){neopixelWrite(NPDI,64,0,64);delay(3000);ws.enable(true);FSYS.begin();})
+		.begin();
+
+	ws.onEvent([](
+		AsyncWebSocket *svr,AsyncWebSocketClient *cli,
+		AwsEventType type,void *arg,
+		uint8_t *w,size_t l
+	){
+		if (type == WS_EVT_CONNECT) {
+		} else if (type == WS_EVT_DISCONNECT) {
+		} else if (type == WS_EVT_ERROR) {
+		} else if (type == WS_EVT_PONG) {
+		} else if (type == WS_EVT_DATA) {
+			escpos.write(w,l);
+			svr->binaryAll(w,l);
+			// AwsFrameInfo *info = (AwsFrameInfo *)arg;
+			// if (info->final && info->index == 0 && info->len == len) {
+			// }
+		}
+	});
+	svr.addHandler(&ws);
+	svr.onNotFound([](AsyncWebServerRequest *r){r->redirect(PUB_DIR);});
+	svr.serveStatic(PUB_DIR,FSYS,PUB_DIR).setDefaultFile("index.html");
+	svr.serveStatic(PVT_DIR,FSYS,PVT_DIR).setDefaultFile("index.html").setAuthentication(NAME,PASS,AsyncAuthType::AUTH_BASIC);
+	svr.begin();
 }
 void loop(){
+	ArduinoOTA.handle();
 	while(Serial.available()){
 		uint8_t x=Serial.read();
 		Serial.write(x);
 		escpos.write(x);
 	}
-	uint8_t x=((!digitalRead(2))<<1)|(!digitalRead(3));
-	if(x!=btn){
-		if(x&0b10)escpos.write("\x1b##SELF");
-		btn=x;
-	}
-	// while(Serial1.available())Serial.write(Serial1.read());
-	neopixelWrite(1,
+	// uint8_t x=((!digitalRead(BTNA))<<1)|(!digitalRead(BTNB));
+	// if(x!=btn){
+	// 	if(x&0b10)escpos.write(P_SELF);
+	// 	btn=x;
+	// }
+	neopixelWrite(NPDI,
 		(sin(millis()/1000.    )*.5+.5)*16.,
 		(sin(millis()/1000.+2.1)*.5+.5)*16.,
 		(sin(millis()/1000.+4.2)*.5+.5)*16.
---
+++ escpos/src/wlan.h
@@ -0,0 +1,78 @@
+#ifndef WLAN_H
+#define WLAN_H
+
+#include <WiFi.h>
+#include <vector>
+
+struct WiFiEntry{String ssid;String psk;};
+typedef std::vector<WiFiEntry> WiFiEntries;
+class WiFiDict{
+	public:
+		WiFiEntries a;
+		WiFiDict(File& w){
+			while(w.available()){
+				String x=w.readStringUntil('\n');
+				x.trim();
+				if(!x.isEmpty()){
+					int n=x.indexOf('\t');
+					if(~n)a.push_back((WiFiEntry){x.substring(0,n),x.substring(n+1)});
+				}
+			}
+		}
+		const WiFiEntry* find(String w){
+			for(const auto& x:a){if(x.ssid==w)return &x;}
+			return nullptr;
+		}
+};
+
+void wait_wifi_conn(){
+	Serial.printf("connecting...\n");
+	for(uint8_t i=10;i&&(WiFi.status()!=WL_CONNECTED);--i)delay(500);
+}
+
+bool wlan(uint8_t npdi,FS& fs,const char* dict,const char* ssid,const char* psk){
+	bool isAPmode=false;
+	neopixelWrite(npdi,16,16,0);
+	WiFi.begin();
+	// WiFi.disconnect();
+	wait_wifi_conn();
+
+	if(WiFi.status()!=WL_CONNECTED){
+		neopixelWrite(npdi,16,4,0);
+		WiFi.disconnect();
+		File f=fs.open(dict);
+		WiFiDict w(f);
+
+		Serial.printf("scanning...\n");
+		uint8_t n=WiFi.scanNetworks();
+		Serial.printf("scan done: n=%d\n",n);
+		neopixelWrite(npdi,16,16,0);
+		for(uint8_t i=0;i<n;++i){
+			Serial.printf("ssid: %s\n",WiFi.SSID(i).c_str());
+			const WiFiEntry* x=w.find(WiFi.SSID(i));
+			if(x){
+				Serial.printf("known ssid\n");
+				WiFi.begin(x->ssid,x->psk);
+				wait_wifi_conn();
+				if(WiFi.status()==WL_CONNECTED)break;
+			}
+		}
+		WiFi.scanDelete();
+	}
+
+	if(WiFi.status()==WL_CONNECTED){
+		Serial.printf("connected!\n");
+		WiFi.setAutoReconnect(true);
+	}else{
+		isAPmode=true;
+		WiFi.softAP(ssid,psk);
+		delay(100);// https://github.com/espressif/arduino-esp32/issues/985
+		const IPAddress ip(192,168,0,1);
+		const IPAddress subnet(255,255,255,0);
+		WiFi.softAPConfig(ip,ip,subnet);
+		Serial.printf("APmode!\n");
+	}
+	return isAPmode;
+}
+
+#endif
