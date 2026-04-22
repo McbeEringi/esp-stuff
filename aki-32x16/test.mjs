5a8386661a1f53a2d5ed0cce4e84cdb815591bfc
McbeEringi <git@mcbeeringi.dev>
2026-04-23 01:07:19 +0900

    -

--- aki-32x16/test.mjs
+++ aki-32x16/test.mjs
@@ -1,4 +1,5 @@
 #!/bin/env bun
+import{png}from'@mcbeeringi/petit/png';
 
 const td=new TextDecoder();
 let w=Bun.argv[2]??'Lorem ipsum であのイーハトーヴォの世界が広がります';// https://github.com/yuru7/moralerspace
@@ -23,11 +24,17 @@ w=await w[Symbol.iterator]().reduce(async(a,x)=>(
 w=[...Array(w[0].length)[Symbol.iterator]().map((_,i)=>w.map(x=>x[w[0].length-1-i]).join(''))];
 // console.log(w.join('\n'));
 
-w=new Blob([
-	`P4 ${w[0].length} ${w.length}\n`,
-	(w=>new Uint8Array(
-		Array(w.length/8)[Symbol.iterator]().map((_,i)=>+`0b${w.slice(i*8,++i*8)}`)
-	))(w.join(''))
-]),
-// await Bun.write('a.pbm',w),
-await Bun.$`magick - a.png<${w}`
+w={
+	// pbm:new Blob([
+	// 	`P4 ${w[0].length} ${w.length}\n`,
+	// 	(w=>new Uint8Array(
+	// 		Array(w.length/8)[Symbol.iterator]().map((_,i)=>+`0b${w.slice(i*8,++i*8)}`)
+	// 	))(w.join(''))
+	// ]),
+	png:png({data:[...w.join('')].map(x=>+x),width:w[0].length,height:w.length,palette:[0x222222,0xff0000]}).toBlob()
+};
+
+// await Bun.write('a.pbm',w.pbm);
+await Bun.write('a.png',w.png);
+
+// await Bun.$`magick - b.png<${w.pbm}`
